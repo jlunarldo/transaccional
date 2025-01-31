6a00828c2da5107fa0e5455e36c0ef764a0a5fec
jlunarldo <luna4053@gmail.com>
2025-01-31 00:52:30 -0300

    Añadiendo componente card más funcionalidad de boton increment y decrement

---
+++ frontend-angular/my-app/src/Product.ts
@@ -0,0 +1,7 @@
+export interface  Product{
+    id:number;
+    nameProduct:string;
+    category:string;
+    priceUnit:number;
+
+}
---
+++ frontend-angular/my-app/src/app/CardComponent/card.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Product } from '../../Product';
+@Component({
+  selector: 'card-component',
+  templateUrl: './card.component.html',
+  standalone: false,
+  styleUrl: './card.component.css'
+})
+
+export class CardComponent {
+   num:number=0;
+
+    productCard:Product={
+        id:1,
+    nameProduct:"two",
+    category:"three",
+    priceUnit:15.00
+    }
+    
+    isDisabled:boolean=true;
+    decrementUnit(){
+        if(this.num>0){
+            this.num--;
+            this.checkDisabled();
+        }
+    }
+    
+    increaseUnit(){
+        this.num++;
+        this.checkDisabled();
+    }
+    checkDisabled():void{
+        this.isDisabled = this.num == 0;
+    }
+}
